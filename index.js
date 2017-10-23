838f4811a58f056cf2fddaa223d437b8079901cd
Shravan Chitragar <shravan.chitragar@gmail.com>
2017-10-22 22:09:51 -0400

    Express REST assignment

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -11,6 +12,8 @@ const app = express();
 
 app.use('/dishes' , dishRouter);
 app.use('/dishes' , dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/promotions', promoRouter);
 
 app.use(bodyParser.json());
 
